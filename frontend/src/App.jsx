98b806054537e7748283eb209fc7f7de094682e3
shubham <shubhamuprade0@gmail.com>
2025-06-14 15:41:09 +0530

    Your message here

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import AuthPage from './components/auth/AuthPage';
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<AuthPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+
       </Routes>
     </div>
   );
