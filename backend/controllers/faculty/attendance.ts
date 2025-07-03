e85c20cdc24ee2c2e36e3ce90d366b8265f5ea2e
Kaustubha Pundir <kaustubhapundir@gmail.com>
2025-07-03 21:20:35 +0530

    refactored faculty/attendance.ts api endpoint by tranferring code logic to controller

---
+++ backend/controllers/faculty/attendance.ts
@@ -0,0 +1,24 @@
+
+import * as db from '#lib/db.ts';
+import * as auth from '#lib/auth.ts';
+
+export const attendance = async (req: any, res: any) => {
+    const faculty = await db.get_user_from_token(req.cookies.session_token);
+    if (!faculty || faculty.type !== 'faculty') return res.status(403).json({ error: 'Only faculty allowed' });
+
+    const { course_id, student_id, date } = req.body;
+    if (!course_id || !student_id || !date) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    try {
+        const att_id = await db.mark_attendance(course_id, student_id, new Date(date), faculty._id!);
+        res.json({ attendance_id: att_id });
+    } catch (err) {
+        if (err instanceof Error) {
+            res.status(400).json({ error: err.message });
+        } else {
+            res.status(400).json({ error: 'Unknown error occurred' });
+        }
+    }
+}
